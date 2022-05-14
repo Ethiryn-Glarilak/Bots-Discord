70f7f8f5d19001db9cc0c7179a33fcddf2775172
Olivier COQUET <olivier.coquet@epita.fr>
2022-05-14 21:14:05 +0200

    feat(reload):add args reload

--- bot/bot/import_option.py
+++ bot/bot/import_option.py
@@ -32,8 +32,6 @@ class Import:
             "parser" : (Import.__parser_get, Import.__parser_set),
         }
 
-        print(args, Import.link.keys())
-
         for key, value in init.items():
             for name in args:
                 list_import = Import.link.get(name, {}).get(key)
